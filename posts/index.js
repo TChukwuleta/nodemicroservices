584b1446955ffc73a2d8051a8f26b334c5d1868d
Tobechi <tobechichukwuleta125@gmail.com>
2022-03-27 08:03:54 -0700

    Second one

--- posts/index.js
+++ posts/index.js
@@ -1,23 +1,37 @@
 const express = require('express')
 const { randomBytes } = require('crypto')
 const app = express()
+const cors = require('cors')
+const axios = require('axios')
 
 const posts = {}
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+app.use(cors())
 
 app.get('/posts', (req, res) => {
     res.send(posts)
 })
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
     const id = randomBytes(4).toString('hex')
     const { title } = req.body
     posts[id] = {
         id, title
     }
+    await axios.post('http://localhost:4005/events', {
+        type: 'PostCreated',
+        data: { 
+            id, title
+        }
+    })
     res.status(201).send(posts[id])
+})
+
+app.post('/events', (req, res) => {
+    console.log('Received Event: ', req.body.type)
+    res.send({})
 })
 
 app.listen(4000, () => {
